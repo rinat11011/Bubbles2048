cdd9f06be475227f6243c2e126a35409085aa0c2
rinat <rinatv@post.bgu.ac.il>
2020-09-04 19:23:01 +0300

    mute-button

---
+++ Assets/Scripts/Mute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mute : MonoBehaviour
+{
+    public Sprite mutePNG;
+    public Sprite unmutePNG;
+    private Sprite srcImage;
+    //private Image image; 
+    public bool muted = false;
+
+    public void start()
+    {
+        srcImage = gameObject.GetComponent<Image>().sprite;
+
+        srcImage = unmutePNG;
+        //GetComponent<Image>().sprite = unmutePNG;
+    }
+
+    public void MuteButton()
+    {
+        if (muted)
+        {
+            // TODO : mute music
+            srcImage = unmutePNG;
+            
+        }
+        else
+        {
+            srcImage = mutePNG;
+            //image.sprite = mutePNG;
+        }
+        this.gameObject.GetComponent<Image>().sprite = srcImage; 
+        muted = !muted;
+    }
+}
